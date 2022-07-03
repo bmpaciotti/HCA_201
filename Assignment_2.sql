c04b14c8e683adf968719083dce3d713131e0868
Brian Paciotti <bmpaciotti@ucdavis.edu>
2022-07-03 09:43:02 -0700

    Commit Assignment 2

--- Assignment_2.sql
+++ Assignment_2.sql
@@ -1,18 +1,16 @@
 
 
-   -- Assignment 2  -- Data Harmonization 
-
-   --  Pick a database.   This is very important!  When you start, use OMOP_TEST 
+-- Assignment 2  -- Data Harmonization 
 
 
-       -- USE OMOP_PROD;
+--  Pick a database.   This is very important!  When you start, use OMOP_TEST 
+ -- USE OMOP_PROD;
 
 
-        USE OMOP_TEST;
+USE OMOP_TEST;
 
+------------------------------------------------------------------------------------------------------------------------------
 
-
--------------------------------------------------------------------------------------------------------------------------------- 
 -- Part I.  Select Columns, Get Counts
 /*
  Select the first and last column from the PERSON table
@@ -24,9 +22,11 @@
 SELECT *
 FROM person LIMIT 1000;
 
+
 SELECT person_id
 	,year_of_birth
 FROM person;
+
 
 SELECT COUNT(person_id) AS num_persons_count
 FROM person p;
@@ -39,51 +39,77 @@ FROM visit_occurrence;
 SELECT COUNT(DISTINCT person_id) AS num_persons_count
 FROM visit_occurrence;
 
+
 -- COUNTS WITH FILTER 
-EXPLAIN
 
 SELECT COUNT(DISTINCT person_id) AS persons_with_condition_count
 FROM condition_occurrence
 WHERE condition_concept_id = 31967
 	AND person_id IS NOT NULL;
 
-EXPLAIN
 
 SELECT COUNT(DISTINCT person_id) AS persons_with_condition_count
 FROM condition_occurrence
 WHERE condition_concept_id = 31967
 	AND person_id = 1;
 
+
+------------------------------------------------------------------------------------------------------------------------------
 -- Part II.  Filtering Rows (WHERE)
-/*
-  To view the data, select the first 1,000 rows from one of the large tables 
-   Write a query to select only male patients from PERSON table 
-	Write a query to select only patients from California in the PERSON table 
-	Select all patient records that have a specific category of diseases such as diabetes 
-	Select rows from the table where the person was born between 1940 and 1950
-	Create a query to select all rows from the CONDITION_OCCURRENCE table with ICD codes 
-o	You can choose other codes if you prefer just document what you did and why 
-	Select patients in CONDITION_OCCURRENCE with CONDITION_CONCEPT_ID = 134736
-*/
+
+
 SELECT *
 FROM person LIMIT 1000;
 
-SELECT COUNT(person_id) AS num_persons
-FROM person
-WHERE gender_concept_id = 8532;
+-- A "between" statement can help filter the data 
+
+SELECT *
+ FROM person where person_id between 1 and 500;
+
+-- example of a Where statment to filter the rows .. but you need to know what these concept codes mean .. see query below and join to concept table 
+SELECT *
+  FROM person 
+    where race_concept_id  = 8527 and gender_concept_id  = 8507;
 
 
+--  Join the concept_id to the concept table so that you can look at labels/descriptions of the codes
 
--- look at data with "GROUP BY" query 
+SELECT
+      p.gender_concept_id,
+      c.concept_name,      
+      COUNT(person_id) AS num_persons
+FROM person p 
+    inner join concept c 
+      on p.gender_concept_id  = c.concept_id
+  GROUP BY 
+      p.gender_concept_id,
+      c.concept_name    ;
+       
+  
+-- Example of a "GROUP BY" query, you can change the whhere statement to filter out specific states
+ 
 SELECT STATE AS state_abbr
 	,COUNT(*) AS num_persons
 FROM person
 LEFT JOIN location ON person.location_id = location.location_id
+WHERE STATE IN ('AK', 'AR', 'CT', 'FL')
 GROUP BY STATE
 ORDER BY STATE;
 
 
+  -- ICD9CM for Condition
+ 
+SELECT concept_id, concept_name, concept_code , domain_id , vocabulary_id 
+FROM concept 
+WHERE domain_id = 'Condition'
+AND vocabulary_id = 'ICD9CM'
 
+ORDER BY concept_name ASC   ;
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------
 -- Part III.  Joins 
 /*
     Join the PERSON table with VISIT_OCCURRENCE using an inner join
@@ -93,18 +119,67 @@ ORDER BY STATE;
 	Try to see how data can be included from one of the tables but can be missing from another table.  
 
 */
+
+
+-- Join person table to the condition table 
+ SELECT DISTINCT co.person_id, c.concept_name
+FROM condition_occurrence co 
+INNER JOIN concept c
+   ON co.condition_concept_id = c.concept_id
+;
+
+
+-- Join to concept table to get condition names
+
+
+SELECT DISTINCT(concept_name) 
+FROM condition_occurrence co
+  inner join concept c 
+    on co.condition_concept_id = c.concept_id;
+    
+
+
 SELECT v.*
 FROM visit_occurrence v
-INNER JOIN person p ON v.person_id = p.person_id
-WHERE p.person_id = 1;
+INNER JOIN person p 
+  ON v.person_id = p.person_id
+  WHERE p.person_id = 1;
+ 
+ 
+ -- try a left join to see if every patient in person table has some visit data 
+ 
+ SELECT p.person_id, v.person_id
+  FROM person p   
+   LEFT JOIN visit_occurrence v
+     ON p.person_id = v.person_id;
+   
+ 
 
---CE10: Counts of persons with conditions
-/*
-Description: This query is used to count the persons with any number of eras of a 
-certain condition (condition_concept_id). The input to the query is a value 
-(or a comma-separated list of values) of a condition_concept_id. 
-If the input is omitted, all possible values are summarized.
-*/
+------------------------------------------------------------------------------------------------------------------------------  
+-- Part IV.  Sorting (Ordering)  
+    
+  
+SELECT person_id
+FROM condition_occurrence
+ORDER BY PERSON_ID;
+
+
+
+SELECT PERSON_ID, visit_start_date , visit_occurrence_id 
+FROM visit_occurrence
+ORDER BY PERSON_ID , visit_start_date ;
+
+
+
+SELECT PERSON_ID, visit_start_date , visit_occurrence_id 
+FROM visit_occurrence
+ORDER BY PERSON_ID DESC , visit_start_date ;
+
+
+
+-- CE10: Counts of persons with conditions
+
+
 SELECT ce.condition_concept_id
 	,c.concept_name
 	,COUNT(DISTINCT person_id) AS num_people
@@ -124,41 +199,20 @@ ORDER BY num_people DESC;
 
 
 
--- Part IV.  Sorting (Ordering)  
--- THIS TAKE A LONG TIME TO RUN.. BE CAREFUL.   LIMIT COLUMNS TO MAKE IT FASTER 
-SELECT *
-FROM condition_occurrence
-ORDER BY PERSON_ID;
 
-SELECT PERSON_ID
-FROM visit_occurrence
-ORDER BY PERSON_ID
+---------------------------------------------------------------------------------------------------------------------------------
+  -- Additional Examples 
 
--- OPTIONAL 
-/*
-  	Select all patients in the PERSON table who less than 80 years old and who have diabetes in the CONDITION_OCCURRENCE table
 
-	Use a join to select specific types of patients based on procedure codes using the PROCEDURE_OCCURRENCE table. Sort the query by PERSON_ID and then VISIT_OCCURRENCE_ID to better understand the output. 
-o	What is a problem with joining tables that both have multiple rows per patient?
+-- use wildcard query to search hypertension related codes
 
-	Create a query that has patients with at least one of the following drugs as defined by RxNorm vocabulary_id (19024574, 19024591, 19024592, 19024593)
+SELECT DISTINCT(concept_name) FROM condition_occurrence, concept
+WHERE condition_occurrence.condition_concept_id = concept.concept_id
+AND concept_name LIKE '%hypertension%';
 
-	Evaluate if any of VISIT_OCCURRENCE_IDs on the MEAUREMENT table matches to encounters on the VISIT_OCCURENCE table 
 
-	Create a "region" variable by grouping at least 15 states into US regions (e.g., Northwest, Southwest, Southeast).  
-o	You can do this using CASE/WHEN statements. 
 
-   Show how to add the labels for gender based on codes in the PERSON table (join to CONCEPT table)
 
-	Provide two examples of how you can use the wildcard function (e.g., '%DIABETES%) to search for particular diseases in the CONCEPT table
-
-	Illustrate at least one way to evaluate if patients have more than one row in the VISIT_OCCURRENCE table
-
-	Show how to calculate some average costs in the COST table
-
-	Using a GROUP BY statement, count how many rows are associated with different visit types in the VISIT_OCCURRENCE table
-
-*/
 SELECT month_of_birth
 	,day_of_birth
 	,count(*) AS num_persons
@@ -178,6 +232,7 @@ ORDER BY COUNT(*) DESC;
 
 
 
+
 SELECT cs.place_of_service_concept_id
 	,count(*) places_of_service_count
 FROM care_site cs
@@ -187,9 +242,11 @@ ORDER BY 1;
 
 
 -- perhaps we interested in the average length of an observation period:
+
 SELECT avg(year(observation_period_start_date) - year(observation_period_end_date) / 365.25) AS NUM_YEARS
 FROM observation_period;
 
+-- Year funtion to select the year from a date
 
 
 SELECT year(observation_period_end_date) AS DT_Year
@@ -197,12 +254,7 @@ SELECT year(observation_period_end_date) AS DT_Year
 FROM observation_period;
 
 
-
-SELECT *
-FROM person
-WHERE GENDER_CONCEPT_ID = 8507;
-
-
+-- max age 
 
 SELECT MAX(year(observation_period_end_date) - year_of_birth) AS max_age
 FROM person
@@ -210,12 +262,49 @@ INNER JOIN observation_period ON person.person_id = observation_period.person_id
 
 
 
+-- look at some averagess 
+
+SELECT 
+ROUND(AVG(total_paid),2) as average_total_paid, 
+ROUND(AVG(paid_by_patient),2) as average_paid_by_patient, 
+ROUND(AVG(total_paid - paid_by_patient),2) AS average_paid_by_other 
+FROM cost ;
+
+
+
+-- select todays date
+SELECT CURDATE();
+
+
+
+-- Create an age field base on today's date 
+ SELECT 
+     year_of_birth ,
+    ( year(curdate()) - year_of_birth)  as Age 
+  FROM person 
+
+
+
+ --  count NULL providers, and % NULL 
+  
+ SELECT SUM(total_rows.null_row) as null_count, 
+         (1.0*SUM(total_rows.null_row)/SUM(total_rows.row_count)) AS percent_null
+
+ FROM
+(
+  SELECT 1 AS row_count, (CASE WHEN v.provider_id IS NULL THEN 1 ELSE 0 END) AS null_row 
+    FROM visit_occurrence v
+    
+) total_rows ;
+  
+  
+---------------------------------------------------------------------------------------------------------------------------------
+  -- More complex examples of SQL
+
+
 
 /* ------------------------------------------------------------------------------------------------------------------ */
-
-
-   -- Here is example of a subquery method 
-
+-- This is an example of a subquery method using the "WITH" statement.  The T query if first run by the computer, then tables from this temporary table are selected below 
 WITH T
 AS (
 	SELECT p.person_id
@@ -226,9 +315,42 @@ AS (
 	INNER JOIN person p ON p.person_id = ce.person_id
 	INNER JOIN concept c1 ON c1.concept_id = ce.condition_concept_id
 	INNER JOIN concept c2 ON c2.concept_id = p.gender_concept_id
+	
 	)
 SELECT person_id
 	,cond
 	,gender
 	,age
 FROM T;
+
+
+
+/*********
+PLAUSIBLE_TEMPORAL_AFTER
+get number of records and the proportion to total number of eligible 
+records with datetimes that do not occur on or after their corresponding datetimes
+Cleaned up by: Kyle P. Rasku RN
+**********/
+
+
+SELECT  violated.visits_before_birth, 
+        CASE WHEN denominator.num_rows = 0 THEN 0 
+		 ELSE 1.0*violated.visits_before_birth/denominator.num_rows 
+		END  AS pct_violated
+FROM
+(
+	SELECT COUNT(visit_start_date) AS visits_before_birth
+    FROM visit_occurrence vo 
+    JOIN person p 
+	ON vo.person_id = p.person_id
+	WHERE p.birth_datetime > vo.visit_start_date
+) violated,
+(
+	SELECT COUNT(*) AS num_rows
+	FROM visit_occurrence vo2 
+) denominator
+;
+
+
+
+
